f92c1d0b87c92d0422ceccc42d68fc7afdd29573
adam ayide <aa22agf@herts.ac.uk>
2023-02-18 15:13:40 +0000

    Creating a new class, booking

---
+++ src/Booking.java
@@ -0,0 +1,24 @@
+public class Booking {
+    private String name;
+    private int age;
+
+    public Booking(String name, int age) {
+        name = "Adam";
+        age = 30;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
