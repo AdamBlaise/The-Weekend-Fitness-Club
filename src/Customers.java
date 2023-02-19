662f6daf59b68037b6e72c75bb92e115826d46a9
adam ayide <aa22agf@herts.ac.uk>
2023-02-19 01:24:53 +0000

    Entering and validating customers details.

--- src/Customers.java
+++ src/Customers.java
@@ -1,6 +1,31 @@
 public class Customers {
-    int id;
-    String name = "Adam";
+    private String name;
+    private String email;
+    private String phone;
+
+    public Customers(String name, String email, String phone) {
+        this.setName(name);
+        this.setEmail(email);
+        this.setPhone(phone);
+    }
+
+    public Customers(){
+        this.name = "Anon";
+        this.email = "ychag@example.com";
+        this.phone = "1234567890";
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return this.email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return this.phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
 
 
 
