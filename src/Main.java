253b5b534b67661106f184c3f50c373baaea020f
adam ayide <aa22agf@herts.ac.uk>
2023-02-19 01:25:29 +0000

    Entering and validating customers details.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    static Scanner input = new Scanner(System.in);
+
+    static Customers customers = new Customers();
+
+    //ENTERING AND VALIDATING CUSTOMER DETAILS
+
+    private static Customers customerDetails() {
+        String custName ="";
+        String custEmail ="";
+        String custPhone ="";
+
+        //Ensure that customer details are entered correctly
+        do{
+            System.out.println("Enter your Name: ");
+            custName = input.nextLine();
+        }while(custName.equals(""));
+        customers.setName(custName);
+
+        System.out.println("Enter your Email: ");
+        custEmail = input.nextLine();
+        customers.setEmail(custEmail);
+
+        do{
+            System.out.println("Enter your Phone Number: ");
+            custName = input.nextLine();
+        }while(custName.equals(""));
+        customers.setPhone(custPhone);
+
+        return customers;
+    }
+
+
     public static void main(String[] args) {
-        Customers customers = new Customers();
-        System.out.println("Hello world!");
-        System.out.println(customers.name);
-        Booking booking = new Booking("adam", 50);
-        System.out.println(booking.getName() + " is " + booking.getAge() + "yrs old");
+
+        customerDetails();
     }
 }
